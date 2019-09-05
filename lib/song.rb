38a1f68d4fbca7526c457ea53684e8bdcb205166
Kevin Hayden <kevin.fhayden@gmail.com>
2019-09-05 01:02:10 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -53,6 +53,15 @@ class Song
     genre_count
   end
   
-  
+  def self.artist_count
+    artist_count = {}
+    @@artists.each do |artist|
+      if artist_count[artist]
+        artist_count += 1
+      else
+        artist_count = 1
+      end
+    end
+  end
 
 end
